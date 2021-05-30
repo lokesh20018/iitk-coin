a7504ebe5956ca56b2e4d121b571b0a017f0798c
Lokesh <lokesh20018@gmail.com>
2021-05-30 22:06:40 +0530

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lokesh20018/iitk-coin
 
 go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.7 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,90 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"log"
-	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type User struct {
+	id       int
+	username string
+}
+
+// SQL Statement for Create Table
+
+func createTable(db *sql.DB) {
+	createStudentTableSQL := `CREATE TABLE student (
+		"hid" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"id" integer,
+		"name" TEXT
+	  );`
+	log.Println("Creating student table...")
+	// Prepare SQL Statement
+	statement, err := db.Prepare(createStudentTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// Execute SQL Statements
+	statement.Exec()
+	log.Println("student table created")
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertStudent(db *sql.DB, code int, name string) {
+	log.Println("Inserting student record ...")
+	insertStudentSQL := `INSERT INTO student(id, name) VALUES (?, ?)`
+	statement, err := db.Prepare(insertStudentSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(code, name)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func displayStudents(db *sql.DB) {
+	row, err := db.Query("SELECT * FROM student ORDER BY name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var hid int
+		var id int
+		var name string
+
+		row.Scan(&hid, &id, &name)
+		log.Println("Student: ", "hid: ", hid, "  ", id, " ", name)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
+	// SQLite is a file based database.
+
+	log.Println("Creating DB")
+	file, err := os.Create("sqlite-database.db") // Create SQLite file
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("sqlite-database.db created")
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()                                     // Defer Closing the database
+	createTable(sqliteDatabase)
+	// Create Database Tables
+	person := User{id: 190460}
+	person.username = "Lokesh Bharati"
+	// INSERT RECORDS
+
+	insertStudent(sqliteDatabase, person.id, person.username)
+
+	// DISPLAY INSERTED RECORDS
+	displayStudents(sqliteDatabase)
 }
